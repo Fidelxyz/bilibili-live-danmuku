7ae2aaa13014dd166a3e95526d757dca79d39e3d
Fidel Yin <fidel.yin@hotmail.com>
2022-04-06 14:33:38 +0800

    update actions

---
+++ scripts/windows-mingw-package.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+param (
+    [string] $archiveName, [string] $targetName
+)
+
+$scriptDir = $PSScriptRoot
+$currentDir = Get-Location
+Write-Host "currentDir" $currentDir
+Write-Host "scriptDir" $scriptDir
+
+function Main() {
+
+    New-Item -ItemType Directory $archiveName # make directory
+    Copy-Item .\build\release\$targetName $archiveName\ # copy exe
+    windeployqt $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName # copy dependency
+
+    $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
+    Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+
+    Compress-Archive -Path $archiveName $archiveName'.zip'
+}
+
+if ($null -eq $archiveName -or $null -eq $targetName) {
+    Write-Host "args missing, archiveName is" $archiveName ", targetName is" $targetName
+    return
+}
+Main
